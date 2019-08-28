e15861086aa3da32ff3a60ea363dabd13e316de4
pablo <pablo_weslly@hotmail.com>
2019-08-28 20:20:27 -0300

    fix: como excluir uma sessao

--- app/index.php
+++ app/index.php
@@ -2,9 +2,13 @@
 
 session_start();
 
+/*
 $_SESSION['usuario'] = [
   'name' => 'Erik',
   'idade' => 32,
   'ativo' => true,
   'rate' => 4.3
-];
+]; */
+
+// Como deletar uma sessão
+unset($_SESSION['usuario']);
